5477f8cb4cf525b79e3eeda930be0b61aa781f08
Hakan Kaya <hakaya@outlook.com>
2023-10-03 20:34:19 +0300

    Initial Commit

---
+++ Gridy/GridyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridyApp.swift
+//  Gridy
+//
+//  Created by Hakan Kaya on 3.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct GridyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
